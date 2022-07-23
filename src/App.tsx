a6e4827a6287d200eff72e1508bfa0c3cda1807b
boyfromgod <manton2007@gmail.com>
2022-07-23 15:11:58 +0300

    add distance receive

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let [currentDistance, setCurrent] = useState<any>(0)
+  async function FrontLite(){
+      let request = await fetch('http://192.168.0.112:4567/api/lamp');
+      if(request.ok){
+          console.log("Done!");
+      }
+  }
+    async function HS_SR04(){
+        let request = await fetch('http://192.168.0.112:4567/api/hssr4');
+        if(request.ok){
+            let r = await request.json();
+            setCurrent(r.distance);
+        }else{
+            console.log("Error");
+        }
+    }
+    setInterval(HS_SR04, 500);
+  async function BackLite(){
+      let request = await fetch('http://192.168.0.112:4567/api/backlamp');
+      if(request.ok){
+          console.log("Done!");
+      }else{
+          console.log("Error");
+      }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={FrontLite}>Front lamp</button>
+        <button onClick={BackLite}>BackLamp</button>
+        <button onClick={HS_SR04}>Дальномер</button>
+        <div>{currentDistance}</div>
     </div>
   );
 }
